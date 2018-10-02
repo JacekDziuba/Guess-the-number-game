7af798272703f085d770bcac1521c3104ccc6a32
jacek <jaco04@wp.pl>
2018-10-02 21:25:40 +0200

    Continuing with the course

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -1,4 +1,4 @@
-package academy.learnprogramming;
+package academy.learnprogramming.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -1,4 +1,4 @@
-package academy.learnprogramming;
+package academy.learnprogramming.console;
 
 import java.util.Random;
 
